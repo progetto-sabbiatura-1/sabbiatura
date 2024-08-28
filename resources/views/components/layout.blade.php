b5ba309e3cd7f267c3ed772f56f7f4eaf0491fbd
Jacopo Bellina <jb.jacopobellina03@gmail.com>
2024-08-28 13:37:51 +0200

    fine titolo sitoweb

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,8 @@
     <link rel="icon" href="img/favicon.png" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BENJI</title>
+    <title>Sabbiature&Verniciature di Benyamin Khabbas
+    </title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
